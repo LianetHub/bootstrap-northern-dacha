3e335a4756a08fdca63f57b42d456e6be8b98e15
lianet <jack.tolmak@yandex.ru>
2026-02-10 11:42:36 +0300

    update media content

--- src/js/app.js
+++ src/js/app.js
@@ -114,36 +114,7 @@ $(function () {
 
     window.addEventListener('resize', () => getHeaderHeight());
 
-    window.addEventListener('wheel', function (event) {
-        if (event.deltaY > 0) {
-            $('header').addClass('scroll');
-        } else {
-            $('header').removeClass('scroll');
-        }
-    });
 
-    let lastScrollTop = 0;
-    $(window).on('scroll', function () {
-        let currentScroll = $(this).scrollTop();
-        if (currentScroll < lastScrollTop) {
-            $('header').removeClass('scroll');
-        }
-        lastScrollTop = currentScroll;
-    });
-
-    $(window).on('scroll', function () {
-        let scrollTop = $(this).scrollTop();
-        let docHeight = $(document).height();
-        let winHeight = $(this).height();
-
-        let scrollPercent = (scrollTop / (docHeight - winHeight)) * 100;
-
-        if (scrollPercent > 66) {
-            $('body').addClass('scrolled-half');
-        } else {
-            $('body').removeClass('scrolled-half');
-        }
-    });
 
 
     // animation
@@ -151,87 +122,6 @@ $(function () {
 
     gsap.registerPlugin(ScrollTrigger);
 
-
-    const promoBanner = document.querySelector(".promo__banner");
-    const promo = document.querySelector(".promo");
-
-    if (promoBanner && promo) {
-
-        gsap.set(promoBanner, {
-            width: window.innerWidth * 0.3333,
-            height: window.innerHeight * 0.1,
-        });
-
-        gsap.to(promoBanner, {
-            width: () => window.innerWidth,
-            height: () => window.innerHeight * 0.5,
-            scrollTrigger: {
-                trigger: promo,
-                start: "top top",
-                end: () => window.innerHeight / 2,
-                scrub: true,
-                pin: false,
-                anticipatePin: 1,
-            },
-            ease: "none",
-            transformOrigin: "bottom center",
-        });
-    }
-
-
-    const directionImages = document.querySelectorAll('.directions__item-image');
-    if (directionImages.length) {
-        directionImages.forEach((image) => {
-            gsap.fromTo(image,
-                { maxWidth: '50%' },
-                {
-                    maxWidth: '100%',
-                    ease: 'none',
-                    scrollTrigger: {
-                        trigger: image,
-                        start: 'top 40%',
-                        end: 'top 20%',
-                        scrub: true,
-                    }
-                }
-            );
-        });
-    }
-
-
-    window.addEventListener('load', () => {
-        const image = document.querySelector('.project__image');
-        const body = document.querySelector('.project__body');
-
-        if (image && body) {
-            gsap.to(image, {
-                height: window.innerHeight * 0.45,
-                duration: 0.6,
-                ease: 'power2.out',
-                onComplete: () => {
-                    gsap.to(image, {
-                        height: 270,
-                        duration: 1,
-                        delay: 0.1,
-                        ease: 'power2.out',
-                        onComplete: () => {
-                            gsap.to(body, {
-                                opacity: 1,
-                                visibility: 'visible',
-                                duration: 0.4,
-                                ease: 'power2.out'
-                            });
-                        }
-                    });
-                }
-            });
-
-            gsap.set(body, {
-                opacity: 0,
-                visibility: 'hidden'
-            });
-        }
-    });
 
 
 
